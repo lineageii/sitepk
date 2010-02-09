89a2d90f4b225cc8dca5e78935550513584a064e
roy hu <hujiag@gmail.com>
2010-02-10 01:15:54 +0800

    update

--- src/main/java/com/sitepk/sample/HelloWorldResource.java
+++ src/main/java/com/sitepk/sample/HelloWorldResource.java
@@ -4,10 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import com.sitepk.entity.Site;
-import com.sun.jersey.api.client.ClientResponse.Status;
 
 @Path("/")
 public class HelloWorldResource {
@@ -22,11 +20,11 @@ public class HelloWorldResource {
 		return site;
 	}
 	
-	@Path("/user/add")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response addUser() {
-		return Response.status(Status.BAD_GATEWAY).build();
-	}
+//	@Path("/user/add")
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response addUser() {
+//		return Response.status(Status.BAD_GATEWAY).build();
+//	}
 	
 }
